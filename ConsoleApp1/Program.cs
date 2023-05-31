8d7b666407f4dd74e597029a984b01d5698b5987
Mechwart András <mechwwart@gmail.com>
2023-05-31 10:05:24 +0200

    Befejezés

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,41 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            List<kemia> elemek = new List<kemia>();
+            using (StreamReader sr = new StreamReader("felfedezesek.csv"))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    kemia elem = new kemia(line);
+                    elemek.Add(elem);
+                }
+            }
+
+            //3
+            Console.WriteLine($"3. feladat: Elemek száma:{elemek.Count()}");
+            //4
+            Console.WriteLine($"4. feladat: Felfedezések az ókorban: {elemek.Where(x => x.Ev == "ókor").Count()}");
+            //5
+
+            string kivVegyjel = "";
+            string[] betuk = { "A","B","C","D","E","F","G","H","I","K","L","M","N","O","P","Q","R","S","T","V","X","Y","Z" };
+
+
+            do
+            {
+              Console.WriteLine("5. Feladat: Kérek egy vegyjelet:");
+                kivVegyjel = Console.ReadLine();
+            } while (kivVegyjel.Length > 2 && !betuk.Contains(kivVegyjel));
+
+            //6
+
+            //elemek.Where(x => x.Vegyjel == kivVegyjel.ToLower()).OrderBy(x => x.Vegyjel).ToList().ForEach(x => Console.WriteLine($"{}"));
+
+            //7
+
+
         }
     }
 }
---
+++ ConsoleApp1/kemia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class kemia
+    {
+        private string ev;
+        private string nev;
+        private string vegyjel;
+        private int rendszam;
+        private string felfedezo;
+
+
+        public kemia(string line)
+        {
+            string[] adatok = line.Split(';');
+            Ev = adatok[0];
+            Nev = adatok[1];
+            Vegyjel = adatok[2];
+            Rendszam = int.Parse(adatok[3]);
+            Felfedezo = adatok[4];
+        }
+        public string Ev { get => ev; set => ev = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public string Vegyjel { get => vegyjel; set => vegyjel = value; }
+        public int Rendszam { get => rendszam; set => rendszam = value; }
+        public string Felfedezo { get => felfedezo; set => felfedezo = value; }
+    }
+}
